1f977230c2c8e7c508facf5e64a4e0dc9c5683e7
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-15 16:40:55 +0300

    Redux setup success. Added reducers and actions. Hooked them up to App and Post component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
 /*
 
 We use react-router-dom a library that is used for in application
@@ -6,22 +9,32 @@ routing in react apps. Using it we can mount components of our
 choice on urls of our choice
 
 */
+
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import postApp from "./reducers";
+
 import Posts from './components/Posts';
 import NotFound from './components/404/NotFound';
 
-// import logo from './logo.svg';
-import './App.css';
+let store = createStore(postApp);
+
+
+
 
 class App extends Component {
   render() {
     return (
+    <Provider store={ store }>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Posts} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
+    </Provider>
     );
   }
 }
--- react-frontend/src/components/Posts.jsx
+++ react-frontend/src/components/Posts.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default class Posts extends Component{
+class Posts extends Component{
   render(){
     return(
       <div>
         <h2>Welcome to Posts</h2>
+        <hr/>
+        <h3>Posts</h3>
+        <table>
+          <tbody>
+            {this.props.posts.map((post, id) =>(
+              <tr key={`post_${id}`}>
+                <td>{post.text}</td>
+                <td><button>edit</button></td>
+                <td><button>delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br/>
         <p>
           Click  <Link to="/contacts">here</Link> to contact us.
         </p>
@@ -13,3 +28,17 @@ export default class Posts extends Component{
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    posts: state.posts,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Posts)
